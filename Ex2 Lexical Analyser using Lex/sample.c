a71608b93694fb665303f23feaf4dc08dc60abbc
Sreyas Venkataramanan <52529980+imsreyas7@users.noreply.github.com>
2021-02-05 15:33:17 +0530

    completed

---
+++ Ex2 Lexical Analyser using Lex/sample.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+//Sample comment 1
+func()
+{
+    int a = 10, b = 20;
+    float c = 9.25;
+    if ( a > b )
+        printf( “a is greater” );
+    else
+        printf( “b is greater” );
+}
+//sample comment 2
+/*
+Multi line comment
+hello there
+sample comment 3
+*/
